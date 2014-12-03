6280dd9ee87197ceb07fb28f73b414d8210b8fbc
Gauthier C <gc@ac-lille.fr>
2014-12-03 13:12:53 +0100

    Add SPICE_PASSWD variable for authentificate remote connect

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
-/usr/bin/Xorg -config /root/spiceqxl.xorg.conf :2 &
 SPICE_USER=${SPICE_USER:-"user"}
 SPICE_UID=${SPICE_UID:-"1000"}
 SPICE_GID=${SPICE_GID:-"1000"}
+SPICE_PASSWD=${SPICE_PASSWD:-"password"}
 useradd -ms /bin/bash -u $SPICE_UID $SPICE_USER
-echo "$SPICE_USER:azerty" | chpasswd
+echo "$SPICE_USER:$SPICE_PASSWD" | chpasswd
+sed -i "s|#Option \"SpicePassword\" \"\"|Option \"SpicePassword\" \"$SPICE_PASSWD\"|" /etc/X11/spiceqxl.xorg.conf
+unset SPICE_PASSWD
+su $SPICE_USER -c "/usr/bin/Xorg -config /etc/X11/spiceqxl.xorg.conf -logfile  /home/$SPICE_USER/.Xorg.2.log :2 &"
 su $SPICE_USER -c "DISPLAY=:2 /usr/bin/startlxde"
+
